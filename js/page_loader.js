7ddafb0109bbac3e0d3c46ea185515f4f36e5f05
GovindPotdar <govindpotdar2001@gmail.com>
2022-07-08 23:29:49 +0530

    added condition to stop nevigating to other page from url - page_loader.js file

--- js/page_loader.js
+++ js/page_loader.js
@@ -1,6 +1,15 @@
+// if try to nevigate to other page then redirect to home page 
+if(location.host == "govindpotdar.github.io"){
+  location.replace("https://govindpotdar.github.io/hey/")
+}
+
+
+// page loader funation
 function changePageContent(fileName){
   $('#page-data').load(`html/${fileName}.html`)
 }
 
+// default home page loader
 $('#page-data').load('html/index.html');
 $('#footer-data').load('html/footer.html');
+
